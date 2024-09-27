0212cdf4f49127d57c26ec8307a3a19b74bfcfd1
M4lf0r <64777390+m4lf0r@users.noreply.github.com>
2024-09-27 17:50:14 -0300

    API

---
+++ PortoDigital_NeuroTech/group1/src/main/java/com/TeamPorto/group1/Group1Application.java
@@ -0,0 +1,13 @@
+package com.TeamPorto.group1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group1Application.class, args);
+	}
+
+}
---
+++ PortoDigital_NeuroTech/group1/src/test/java/com/TeamPorto/group1/Group1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TeamPorto.group1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
